dd8c4fd50b4aa638ec7ba2b42723c066af1cc1bb
Benjie Gillam <benjie@jemjie.com>
2018-01-22 12:38:07 +0000

    Modify UnicodeUtils to embed license / remove dependency

--- src/UnicodeUtils.js
+++ src/UnicodeUtils.js
@@ -1,11 +1,35 @@
+/*
+ * This is an almost verbatim copy of
+ *
+ * https://github.com/facebook/fbjs/blob/7da8335b78d669cba263760872f0a45ed16b4d12/packages/fbjs/src/unicode/UnicodeUtils.js
+ *
+ * It has been modified to remove `invariant` dependency and to embed the license directly.
+ */
+
 /**
  * Copyright (c) 2013-present, Facebook, Inc.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * MIT License
  *
- * @providesModule UnicodeUtils
- * @typechecks
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 /**
@@ -21,7 +45,18 @@
 
 'use strict';
 
-const invariant = require('invariant');
+const invariant = function (condition, format, a, b) {
+  if (!condition) {
+    const args = [a, b];
+    let argIndex = 0;
+    const error = new Error(
+      format.replace(/%s/g, function () { return args[argIndex++]; })
+    );
+    error.name = 'Invariant Violation';
+    error.framesToPop = 1;
+    throw error;
+  }
+}
 
 // These two ranges are consecutive so anything in [HIGH_START, LOW_END] is a
 // surrogate code unit.
